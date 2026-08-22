a0f574b702fa991126d642957d7fcb1fa79067ff
Erick Alcantara <jorgerickal@gmail.com>
2026-08-21 20:27:20 -0500

    Ejercicio Mejorado realizado con metodo del proyecto final

--- src/main/java/www/cibertec/Main.java
+++ src/main/java/www/cibertec/Main.java
@@ -3,16 +3,64 @@ package www.cibertec;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    // 1. Metodo
-    public static double calcularPromedio(double n1, double n2) {
-        return (n1 + n2) / 2.0;
-    }
 
     public static void main(String[] args) {
-        // 2. Variables Fijas
-        String alumno = "Erick Alcantara";
-        double promedioFinal = calcularPromedio(16.0, 11.0);
 
-        System.out.println("El promedio final de " + alumno + " es: " + promedioFinal);
+        //  Variables fijas (String, int, double, boolean)
+        String alumno = "Erick Alcantara";
+        double nota1 = 18.0;
+        double nota2 = 16.5;
+        double nota3 = 14.0;
+        int faltas = 3;
+        boolean aprobo;
+
+        //   Método Calcular Promedio
+        double promedio = calcularPromedio(nota1, nota2, nota3);
+
+        //  Estructura condicional y multiple
+
+        int condicionFinal;
+
+        if (promedio >= 13 && faltas <= 4) {
+            condicionFinal = 1; // Aprobado
+            aprobo = true;
+        } else if (promedio >= 10 && promedio < 13) {
+            condicionFinal = 2; // Recuperación
+            aprobo = false;
+        } else {
+            condicionFinal = 3; // Desaprobado
+            aprobo = false;
+        }
+
+        // Imprimir resultados usando Switch Case
+        System.out.println("\n=== RESULTADO FINAL ===");
+        System.out.println("Alumno: " + alumno);
+        System.out.println("Promedio: " + promedio);
+
+        switch (condicionFinal) {
+            case 1:
+                System.out.println("Estado: ¡APROBADO FELICITACIONES!");
+                break;
+            case 2:
+                System.out.println("Estado: PASA A EXAMEN DE RECUPERACIÓN");
+                break;
+            case 3:
+                System.out.println("Estado: DESAPROBADO");
+                break;
+        }
+
+        //  (while) - Un conteo simple de advertencia
+        System.out.print("Alertas registradas: ");
+        int i = 1;
+        while (i <= faltas) {
+            System.out.print("[Falta " + i + "] ");
+            i++;
+        }
+        System.out.println();
+    }
+
+    //  Método - Operación básica con retorno
+    public static double calcularPromedio(double n1, double n2, double n3) {
+        return (n1 + n2 + n3) / 3.0;
     }
 }
